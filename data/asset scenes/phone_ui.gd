6ae5c101a58d2800419079f8511f78360bdef387
CultOfApe <ted.project.bcn@gmail.com>
2018-05-18 01:29:55 +0200

    More Phone UI preparatory work
    created sub screens (which contain nothing but a header and can´t be exited yet) for when icons are clicked, and a wobbly effect when hovering the icons.

--- data/asset scenes/phone_ui.gd
+++ data/asset scenes/phone_ui.gd
@@ -14,86 +14,94 @@ func _ready():
 #	# Update game logic here.
 #	pass
 
+func icon_fx(node, scale):
+	$fx.interpolate_property (node, "scale", node.scale, scale, 0.5, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
+	$fx.start()
 
-func _on_area_mouse_entered():
-	pass # replace with function body
-
-
-func _on_area_mouse_exited():
-	pass # replace with function body
-
-
-func _on_area_input_event():
-	pass # replace with function body
-
-
-func _on_phone_mouse_entered(body_id, body, body_shape, area_shape):
-	pass # replace with function body
+func _on_phone_mouse_entered():
+	icon_fx($homescreen/phone, Vector2(0.9, 0.9))
 
 
 func _on_phone_mouse_exited():
-	pass # replace with function body
+	icon_fx($homescreen/phone, Vector2(1, 1))
+
+func _on_phone_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.is_pressed():
+				$ui_phone.show()
 
 
-func _on_phone_input_event():
-	pass # replace with function body
+func _on_mail_mouse_entered():
+	icon_fx($homescreen/mail, Vector2(0.9, 0.9))
 
 
-func _on_mail_mouse_entered(area_id, area, area_shape, self_shape):
-	pass # replace with function body
+func _on_mail_mouse_exited():
+	icon_fx($homescreen/mail, Vector2(1, 1))
 
 
-func _on_mail_mouse_exited(body_id, body, body_shape, area_shape):
-	pass # replace with function body
-
-
-func _on_mail_input_event():
-	pass # replace with function body
+func _on_mail_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.is_pressed():
+				$ui_phone.show()
 
 
 func _on_internet_mouse_entered():
-	pass # replace with function body
+	icon_fx($homescreen/internet, Vector2(0.9, 0.9))
 
 
 func _on_internet_mouse_exited():
-	pass # replace with function body
+	icon_fx($homescreen/internet, Vector2(1, 1))
 
 
-func _on_internet_input_event():
-	pass # replace with function body
+func _on_internet_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.is_pressed():
+				$ui_internet.show()
 
 
-func _on_archive_mouse_entered(body_id, body, body_shape, area_shape):
-	pass # replace with function body
+func _on_archive_mouse_entered():
+	icon_fx($homescreen/archive, Vector2(0.9, 0.9))
 
 
 func _on_archive_mouse_exited():
-	pass # replace with function body
+	icon_fx($homescreen/archive, Vector2(1, 1))
 
 
-func _on_archive_input_event():
-	pass # replace with function body
+func _on_archive_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.is_pressed():
+				$ui_archive.show()
 
 
 func _on_calendar_mouse_entered():
-	pass # replace with function body
+	icon_fx($homescreen/calendar, Vector2(0.9, 0.9))
 
 
 func _on_calendar_mouse_exited():
-	pass # replace with function body
+	icon_fx($homescreen/calendar, Vector2(1, 1))
 
 
-func _on_calendar_input_event():
-	pass # replace with function body
+func _on_calendar_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.is_pressed():
+				$ui_calendar.show()
 
 
 func _on_games_mouse_entered():
-	pass # replace with function body
+	icon_fx($homescreen/games, Vector2(0.9, 0.9))
 
 
-func _on_games_mouse_exited(area):
-	pass # replace with function body
+func _on_games_mouse_exited():
+	icon_fx($homescreen/games, Vector2(1, 1))
 
 
-func _on_games_input_event():
-	pass # replace with function body
+func _on_games_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.is_pressed():
+				$ui_games.show()
