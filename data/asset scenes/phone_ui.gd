b0909736a2e54c86cc2584da04cfc6099159474d
CultOfApe <ted.project.bcn@gmail.com>
2018-05-17 20:22:44 +0200

    Groundwork for phone UI overlay
    Prepared some placeholder (!) graphics and icons and setup collison areas attached to phone_ui.gd. No functionality yet, though.
    
    Just to give me a head start for the weekend code sprint.

---
+++ data/asset scenes/phone_ui.gd
@@ -0,0 +1,99 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here.
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_area_mouse_entered():
+	pass # replace with function body
+
+
+func _on_area_mouse_exited():
+	pass # replace with function body
+
+
+func _on_area_input_event():
+	pass # replace with function body
+
+
+func _on_phone_mouse_entered(body_id, body, body_shape, area_shape):
+	pass # replace with function body
+
+
+func _on_phone_mouse_exited():
+	pass # replace with function body
+
+
+func _on_phone_input_event():
+	pass # replace with function body
+
+
+func _on_mail_mouse_entered(area_id, area, area_shape, self_shape):
+	pass # replace with function body
+
+
+func _on_mail_mouse_exited(body_id, body, body_shape, area_shape):
+	pass # replace with function body
+
+
+func _on_mail_input_event():
+	pass # replace with function body
+
+
+func _on_internet_mouse_entered():
+	pass # replace with function body
+
+
+func _on_internet_mouse_exited():
+	pass # replace with function body
+
+
+func _on_internet_input_event():
+	pass # replace with function body
+
+
+func _on_archive_mouse_entered(body_id, body, body_shape, area_shape):
+	pass # replace with function body
+
+
+func _on_archive_mouse_exited():
+	pass # replace with function body
+
+
+func _on_archive_input_event():
+	pass # replace with function body
+
+
+func _on_calendar_mouse_entered():
+	pass # replace with function body
+
+
+func _on_calendar_mouse_exited():
+	pass # replace with function body
+
+
+func _on_calendar_input_event():
+	pass # replace with function body
+
+
+func _on_games_mouse_entered():
+	pass # replace with function body
+
+
+func _on_games_mouse_exited(area):
+	pass # replace with function body
+
+
+func _on_games_input_event():
+	pass # replace with function body
