a70f87596e173e1d5de5e0e3089c6080bcaf3ec0
CultOfApe <ted.project.bcn@gmail.com>
2018-06-02 19:50:20 +0200

    Fixing small inconsistencies in UX - Part 2
    small tweak. You shouldn´t even be able to highlight current location on map.

--- data/scripts/map_ui.gd
+++ data/scripts/map_ui.gd
@@ -14,10 +14,11 @@ func _ready():
 	pass
 
 func _on_map01_mouse_entered():
-	map01.show()
-	label.set_text("School yard")
-	label.set_position($map01.get_position() + Vector2(-30, 45))
-	global.blocking_ui = true
+	if global.currentLocation != "schoolyard":
+		map01.show()
+		label.set_text("School yard")
+		label.set_position($map01.get_position() + Vector2(-30, 45))
+		global.blocking_ui = true
 
 func _on_map01_mouse_exited():
 	map01.hide()
@@ -29,10 +30,11 @@ func _on_map01_input_event(viewport, event, shape_idx):
 			emit_signal("location_chosen", "schoolyard")
 
 func _on_map02_mouse_entered():
-	map02.show()
-	label.set_text("School hall")
-	label.set_position($map02.get_position() + Vector2(-30, 45))
-	global.blocking_ui = true
+	if global.currentLocation != "schoolhall":
+		map02.show()
+		label.set_text("School hall")
+		label.set_position($map02.get_position() + Vector2(-30, 45))
+		global.blocking_ui = true
 
 func _on_map02_mouse_exited():
 	map02.hide()
@@ -44,10 +46,11 @@ func _on_map02_input_event(viewport, event, shape_idx):
 			emit_signal("location_chosen", "schoolhall")
 
 func _on_map03_mouse_entered():
-	map03.show()
-	label.set_text("My room")
-	label.set_position($map03.get_position() + Vector2(-30, 45))
-	global.blocking_ui = true
+	if global.currentLocation != "myroom":
+		map03.show()
+		label.set_text("My room")
+		label.set_position($map03.get_position() + Vector2(-30, 45))
+		global.blocking_ui = true
 
 func _on_map03_mouse_exited():
 	map03.hide()
